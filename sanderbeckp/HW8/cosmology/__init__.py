4b14aaf74a0f205d45599d0f5a02d80d2df402e8
Phoebe Upton Sanderbeck <Phoebe@Phoebes-MacBook-Pro.local>
2013-11-28 23:21:18 -0800

    Commit HW8

---
+++ sanderbeckp/HW8/cosmology/__init__.py
@@ -0,0 +1,8 @@
+"""Cosmology tools
+
+Calculate different cosmological distance quantities using expressions from Hogg 1999. Calculates DM, DC, DA, DL, and the distance modulus. 
+"""
+# We'll import the Cosmology class using
+# a "relative import":
+from .cosmology import Cosmology
+
